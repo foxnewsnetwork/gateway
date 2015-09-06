d6b300e4ef36b1db35c74db611af42a647d66942
Thomas Chen <foxnewsnetwork@gmail.com>
2015-09-05 20:38:49 -0700

    as per this gist: https://gist.github.com/foxnewsnetwork/bbf373486f557f39902f fixed issue where code loading in production would cause module attributes to not be properly loaded

--- lib/gateway.ex
+++ lib/gateway.ex
@@ -2,15 +2,18 @@ defmodule Gateway do
   defmacro __using__(name_keys\\[]) do
     quote location: :keep do
       use HTTPoison.Base
-      @gateway_config Application.get_env(:gateway, __MODULE__)
+
+      def gateway_config do
+        Application.get_env(:gateway, __MODULE__)
+      end
 
       def process_url(path) do
-        @gateway_config[:url] <> path
+        gateway_config[:url] |> Path.join(path)
       end
     
       def process_request_headers(headers) do
         unquote(name_keys)
-        |> Enum.map(fn {name, key} -> {name, @gateway_config |> Dict.get(key)} end)
+        |> Enum.map(fn {name, key} -> {name, gateway_config |> Dict.get(key)} end)
         |> Kernel.++(headers)
       end
     end
